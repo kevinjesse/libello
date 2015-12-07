e5be9c21f73c2f7673aaae1c2470e6df40e072e8
Kevin Jesse <kjesse@ucsc.edu>
2015-12-06 19:20:11 -0800

    Completed search, buyer page, and email to seller. All functionality completed. Pending some css changes from Vanessa and Daryl.

--- web2py_libello/applications/exchange/controllers/default.py
+++ web2py_libello/applications/exchange/controllers/default.py
@@ -7,6 +7,12 @@ import isbnlib
 import json
 
 
+from gluon.tools import Mail
+mail = Mail()
+mail.settings.server = 'smtp.gmail.com:587'
+mail.settings.sender = 'LibelloExchange@gmail.com'
+mail.settings.login = 'LibelloExchange@gmail.com:libello7!'
+
 
 def reset_table():
     """
@@ -14,7 +20,10 @@ def reset_table():
     """
     db.books.drop()
 
+# def reset_book():
+#     db(db.books.isbn ==  '9781408834985').delete()
 
+@auth.requires_signature()
 def info_isbn():
     isbn = request.vars.isbn
     data = isbnlib.meta(isbn, service='merge')
@@ -22,16 +31,50 @@ def info_isbn():
     desc = isbnlib.desc(isbn)
     return response.json(dict(cover=cover, meta=data, desc=desc))
 
-
 def load_books():
-    rows = db(db.books.id >= 0).select()
-    d = {r.id: {'title': r.title, 'authors': r.authors, 'year': r.year_pub, 'isbn': r.isbn,
-                'pub': r.publisher, 'price': r.price, 'cond': r.cond, 'cover': r.cover,
-                'desc': r.descript, 'user_id': r.user_id}
-         for r in rows}
-    return dict(books=d)
+    book_json = []
+    isbns = []
+    book_infos = db().select(db.books.title, db.books.authors, db.books.year_pub, db.books.isbn, db.books.publisher,
+                       db.books.cover, db.books.descript, db.books.id)
+    for temp_book in book_infos:
+        user_infos = db(db.books.isbn == temp_book['isbn']).select(db.books.price, db.books.cond, db.books.user_id)
+        sellers = []
+        for temp_user in user_infos:
+            sellers.append(dict(temp_user))
+    #prevents multiple books from being added using the list of isbns
+        if temp_book['isbn'] not in isbns:
+             temp_book['sellers'] = sellers
+             temp_book.pop('delete_record')
+             temp_book.pop('update_record')
+             book_json.append(dict(temp_book))
+             isbns.append(temp_book['isbn'])
+    # import pprint
+    # logger.error(pprint.pprint(book_json))
+    return response.json(book_json)
 
+# def load_books():
+#     books = {}
+#     book_infos = db().select(db.books.title, db.books.authors, db.books.year_pub, db.books.isbn, db.books.publisher,
+#                        db.books.cover, db.books.descript)
+#     for temp_book in book_infos:
+#         user_infos = db(db.books.isbn == temp_book['isbn']).select(db.books.price, db.books.cond, db.books.user_id)
+#         sellers = {}
+#         for temp_user in user_infos:
+#             sellers[temp_user['user_id']] = dict(temp_user)
+#
+#         if temp_book['isbn'] not in books:
+#             temp_book['sellers'] = sellers
+#             book = {temp_book['isbn']: dict(temp_book)}
+#             books.update(book)
+#         else:
+#             book = books[temp_book['isbn']]
+#             book['sellers'].update(sellers)
+#             books[temp_book['isbn']] = book
+#     import pprint
+#     logger.error(books)
+#     return response.json(books)
 
+@auth.requires_signature()
 def info_search():
     search = str(request.vars.search)
     json_result = isbnlib.goom(search)
@@ -46,19 +89,71 @@ def info_search():
         search_return.append(temp)
     return response.json(search_return)
 
-
+@auth.requires_signature()
 def verify_isbn():
     isbn = request.vars.isbn.strip()
     return isbnlib.is_isbn10(isbn) or isbnlib.is_isbn13(isbn)
 
 
+@auth.requires_signature()
+def contact_seller():
+    user_id = request.vars.user_id
+    isbn = request.vars.isbn
+    price = request.vars.price
+    cond = request.vars.cond
+    # do lookup of email from this
+    book = db((db.books.isbn == isbn) & (db.books.user_id == user_id) &
+              (db.books.price == price) & (db.books.cond == cond)).select(db.books.title, db.books.authors,
+                                                                              db.books.isbn, db.books.year_pub, db.books.cond,
+                                                                              db.books.price, limitby=(0, 1))
+
+    user_info = db((db.auth_user.id == user_id)).select(db.auth_user.email, db.auth_user.first_name, db.auth_user.last_name)
+
+    curr_user = db((db.auth_user.id == auth.user_id)).select(db.auth_user.email)
+
+    # first = ""
+    # last = ""
+    for each in book:
+        title = each['title']
+        authors = ', '.join(each['authors'])
+        isbn = each['isbn']
+        year = each['year_pub']
+        cond = each['cond']
+        price = each['price']
+        if cond == 4:
+            cond = "Like New"
+        elif cond == 3:
+            cond = "Very Good"
+        elif cond == 2:
+            cond = "Good"
+        elif cond == 1:
+            cond = "Acceptable"
+
+
+    for each in user_info:
+        email = each['email']
+        first = each['first_name']
+        last = each['last_name']
+
+    for each in curr_user:
+        customer = each['email']
+
+    mail.send(to=email,
+          subject='Libello Book Exchange: ' + title,
+          # If reply_to is omitted, then mail.settings.sender is used
+          reply_to=email,
+          message="Hello " + first + " " + last + ",\n\n" + customer + ' is interested in: "' + title + "("+ year + ") by " +
+     authors + '" for $' + price + " in " + cond + " condition.\n\n Thanks,\n The Libello Team")
+    return "ok"
+
+
 def index():
-    return load_books()
+    return dict(user_id=auth.user_id)
 
-
+@auth.requires_signature()
 def book_add():
     title = request.vars['meta[Title]']
-    authors = request.vars['meta[Authors][]']
+    authors = request.vars['meta[Authors]']
     year = request.vars['meta[Year]']
     isbn = request.vars['meta[ISBN-13]']
     pub = request.vars['meta[Publisher]']
@@ -66,105 +161,19 @@ def book_add():
     cond = request.vars['cond']
     cover = ''.join(request.vars['cover[]'])
     desc = request.vars['desc']
-    db.books.insert(title=title, authors=authors,year_pub=year, isbn=isbn, publisher=pub,
-                    price=price, cond=cond, cover=cover, descript=desc, user_id=auth.user_id)
-    return
+    # db.books.update_or_insert(((db.books.isbn == isbn) & (db.books.user_id == auth.user_id)),title=title,
+    #                           authors=authors, year_pub=year, isbn=isbn, publisher=pub, price=price, cond=cond,
+    #                           cover=cover, descript=desc, user_id=auth.user_id)
+    db.books.insert(title=title, authors=authors, year_pub=year, isbn=isbn, publisher=pub, price=price, cond=cond,
+                               cover=cover, descript=desc, user_id=auth.user_id)
+    return "ok"
 
+@auth.requires_signature()
+def book_delete():
+    book_id = request.vars.id
 
-
-#
-# def load_boards():
-#     """Loads all boards for the user."""
-#     rows = db(db.boards.id >= 0).select()
-#     d = {r.board_id: {'board_name': r.board_name, 'author': r.author}
-#          for r in rows}
-#     return response.json(dict(board_dict=d))
-#
-# @auth.requires_signature()
-# def add_board():
-#     db.boards.update_or_insert((db.boards.board_id == request.vars.board_id),
-#             board_id=request.vars.board_id,
-#             board_name=request.vars.board_name,
-#             author=auth.user_id)
-#     return "ok"
-#
-# @auth.requires_signature()
-# def delete_board():
-#     db(db.boards.board_id == request.vars.board_id).delete()
-#     return "ok"
-#
-# @auth.requires_signature()
-# def edit_board():
-#     db(db.boards.board_id == request.vars.board_id).update(board_name=request.vars.board_name)
-#     return "ok"
-#
-# def posts():
-#     board_id = request.args(0)
-#     post_id = gluon_utils.web2py_uuid()
-#     return dict(board_id=board_id, post_id=post_id)
-#
-#
-# def load_posts():
-#     rows = db(db.posts.board_id == request.vars.board_id).select()
-#     d = {r.post_id: {'post_content': r.post_content, 'post_name': r.post_name, 'board_id': r.board_id, 'author': r.author}
-#          for r in rows}
-#     return response.json(dict(post_dict=d))
-#
-#
-# @auth.requires_signature()
-# def add_post():
-#     db.posts.update_or_insert((db.posts.post_id == request.vars.post_id),
-#             post_id=request.vars.post_id,
-#             post_name=request.vars.post_name,
-#             post_content=request.vars.post_content,
-#             board_id=request.vars.board_id,
-#             author=auth.user_id)
-#     return "ok"
-#
-# @auth.requires_signature()
-# def delete_post():
-#     db(db.posts.post_id == request.vars.post_id).delete()
-#     return "ok"
-#
-# @auth.requires_signature()
-# def edit_post_name():
-#     db(db.posts.post_id == request.vars.post_id).update(post_name=request.vars.post_name)
-#     return "ok"
-#
-# @auth.requires_signature()
-# def edit_post_content():
-#     db(db.posts.post_id == request.vars.post_id).update(post_content=request.vars.post_content)
-#     return "ok"
-
-
-#
-# @auth.requires_signature()
-# def delete_posts():
-#     idlist = request.vars['list[]']
-#     if type(idlist) is list:
-#         for identifier in idlist :
-#             db(db.posts.post_id == identifier).delete()
-#     elif type(idlist) is str:
-#         db(db.posts.post_id == idlist).delete()
-#     return "ok"
-#
-# @auth.requires_signature()
-# def edit_posts():
-#     db(db.posts.post_id == request.vars.post_id).update(post_content=request.vars.post_content)
-#     return "ok"
-#
-# @auth.requires_signature()
-# def edit_postsd():
-#     db(db.posts.post_id == request.vars.post_id).update(post_description=request.vars.post_description)
-#     return "ok"
-#
-#
-#
-
-
-
-
-
+    db(db.books.id == book_id).delete()
+    return "ok"
 
 
 def user():
